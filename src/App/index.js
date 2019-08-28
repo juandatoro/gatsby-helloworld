64983b2b033c5459cb242aa8bc9a5b63dbae155d
Juan Restrepo <juan.restrepo@globant.com>
2019-08-27 22:46:55 -0500

    Adding navbar

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,161 @@
 import React from 'react';
+import NavBar from '../Components/Navbar';
+
 import './App.scss';
 
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Clean Gatsby App</h1>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <h1>Hola Mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        voluptate, consectetur rerum, aspernatur blanditiis ipsum dolorum sit
+        nesciunt, enim velit laudantium veritatis ex. Doloribus fugit nulla
+        itaque quas, nesciunt tenetur.
+      </p>
+      <p>
+        Repellendus voluptatem dignissimos animi libero adipisci placeat optio
+        laborum, explicabo deserunt quae veritatis molestiae ex eum sapiente
+        eius? Voluptatem placeat asperiores quam adipisci blanditiis praesentium
+        aut officiis omnis, assumenda mollitia?
+      </p>
+      <p>
+        Hic, saepe, eaque commodi quas adipisci asperiores excepturi in ducimus
+        quaerat aspernatur voluptatibus tempore iure et ut repudiandae, possimus
+        placeat magni suscipit laudantium? Illo atque voluptates, eligendi vitae
+        vel expedita?
+      </p>
+      <p>
+        Ipsum ipsam quod, distinctio facilis quos eveniet ab saepe quia tempora
+        mollitia perspiciatis, dignissimos blanditiis hic officia. Hic inventore
+        alias, soluta nihil sint excepturi vero placeat aliquid perferendis
+        accusamus debitis.
+      </p>
+      <p>
+        Laudantium, officia sunt doloremque consequatur expedita fugiat in aut
+        rem accusamus suscipit, quisquam voluptatem ad tempora dolor ratione
+        unde incidunt obcaecati excepturi debitis aperiam ullam vero, quae
+        aliquam! Voluptate, accusamus.
+      </p>
+      <p>
+        Fuga obcaecati ab dolore! Fugit assumenda, possimus enim placeat natus
+        magni praesentium iste accusamus ut, quod fugiat debitis pariatur cumque
+        maiores eum consectetur omnis? Est blanditiis ducimus aspernatur maiores
+        fugit.
+      </p>
+      <p>
+        Labore, culpa suscipit quas voluptatem expedita excepturi rem sunt vero
+        provident illo tempora placeat iusto deserunt harum delectus? Porro
+        aperiam neque id rem voluptatibus minima dolor. Eius tempore facere cum!
+      </p>
+      <p>
+        Sequi pariatur recusandae corrupti eum iusto minus autem, beatae nam
+        distinctio est ea aut aliquid quaerat quos laudantium, odit ipsa
+        eligendi aliquam blanditiis repudiandae illum! Dolores nobis suscipit
+        aliquam tenetur.
+      </p>
+      <p>
+        Illum nulla sapiente fugiat laudantium quos necessitatibus et commodi
+        cupiditate cum tempore? Ut atque error quibusdam quam accusantium,
+        consequatur, iste architecto fuga sapiente, iure tenetur animi unde in
+        soluta aperiam!
+      </p>
+      <p>
+        Mollitia praesentium, tempore accusamus voluptates quam ullam aperiam
+        quos iure dolor laudantium quo. Laboriosam, veritatis quisquam?
+        Repellendus in a id delectus. Dolor laboriosam quam reprehenderit quia
+        minima deserunt corrupti adipisci!
+      </p>
+      <p>
+        Consectetur expedita quos est quae laborum ut rerum hic! Unde ea facere
+        quos distinctio sed. Recusandae, odio temporibus, ut, fugit inventore
+        atque quisquam facilis nulla qui culpa animi repellendus optio!
+      </p>
+      <p>
+        Magni neque asperiores dicta reiciendis, nostrum laborum minus ducimus
+        facere. Culpa, suscipit debitis? Est possimus, facilis expedita
+        doloribus quia porro officia esse asperiores? Architecto, voluptate
+        corrupti magni exercitationem quas voluptatem?
+      </p>
+      <p>
+        Assumenda neque iusto doloremque atque consectetur illum. Enim quas
+        animi reiciendis velit illo itaque quod suscipit odit ratione, ullam at
+        porro beatae earum quisquam veritatis, consectetur in, soluta voluptates
+        atque?
+      </p>
+      <p>
+        Suscipit aliquam quos rerum? Ullam impedit adipisci fuga ratione. Iure
+        cupiditate similique ex tenetur cum nobis quae fugiat quasi beatae
+        culpa, quidem rem, consequatur provident minima accusamus optio unde
+        minus.
+      </p>
+      <p>
+        Earum, quis tenetur veniam possimus sunt fugiat dicta, obcaecati minima
+        voluptates nam delectus, et facilis similique unde iusto consequuntur
+        neque quos laudantium? Aperiam vero suscipit molestiae pariatur fugiat
+        animi quo?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam,
+        praesentium. Dolores consectetur voluptates, eos facere, accusamus totam
+        molestiae minus amet harum eum expedita maiores ipsam ex odit a dolorum
+        laboriosam.
+      </p>
+      <p>
+        Quibusdam consequuntur impedit voluptates, dicta provident maxime
+        aspernatur fugiat voluptatem temporibus odio ducimus nesciunt error
+        atque distinctio necessitatibus ipsam rerum sint quas mollitia! Porro
+        vel eum, distinctio incidunt modi quas?
+      </p>
+      <p>
+        Reprehenderit asperiores est rerum, qui explicabo ab deserunt. Fugit
+        placeat nesciunt at, voluptate recusandae doloribus dignissimos maxime
+        error nobis tempora dolor eos rerum facilis neque eveniet laboriosam
+        quisquam ut officiis.
+      </p>
+      <p>
+        Nulla impedit sapiente corporis ab, deleniti corrupti doloribus rerum
+        non voluptate itaque necessitatibus perspiciatis qui dolorum libero,
+        distinctio laudantium similique. Rem sit rerum eveniet itaque, ducimus
+        est dolore eaque iste.
+      </p>
+      <p>
+        Voluptate perferendis officiis reprehenderit quibusdam corporis,
+        blanditiis totam qui repellendus ex, iste dolorum exercitationem soluta
+        itaque et natus rem fugiat consequatur vel! Voluptates est voluptatum
+        modi repudiandae hic ipsa quam.
+      </p>
+      <p>
+        Eligendi, maiores. Provident harum deleniti cumque molestiae sed tenetur
+        laborum illo voluptas hic architecto voluptates, delectus, quod placeat
+        nulla vero! Qui, quos deleniti! Nemo nisi fugiat, recusandae eveniet
+        quaerat quis!
+      </p>
+      <p>
+        Error, suscipit voluptates! Temporibus praesentium consequatur
+        accusantium hic sit maiores dolore dolor dolorem? Enim sint quos quo
+        incidunt delectus consequuntur numquam nemo. Repellat quia tempore
+        aperiam, porro officiis suscipit ipsam.
+      </p>
+      <p>
+        Facilis tempore veniam numquam dolore maiores minima, ipsam at a, alias
+        amet recusandae enim. Libero tenetur accusantium, a fugit non, totam
+        ipsa reiciendis odit est qui neque tempora, iste labore?
+      </p>
+      <p>
+        Deleniti quibusdam minima recusandae corporis ducimus quidem fugiat sit
+        impedit, totam perspiciatis suscipit adipisci libero fuga animi, id
+        fugit. Sint dignissimos ex nam et nulla vero consequatur dolore
+        repellendus rem?
+      </p>
+      <p>
+        Ipsam exercitationem, porro deleniti nihil hic tenetur. Quod dicta a
+        placeat facilis iste fuga soluta excepturi dolorem? Quae quia aut minus
+        aliquid, ratione totam inventore modi dignissimos alias, molestiae
+        facilis.
+      </p>
+    </>
   );
 };
 
