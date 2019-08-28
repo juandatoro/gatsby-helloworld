6e25f80981e21a9da20b73cb206cc5fb692bed7e
Juan Restrepo <juan.restrepo@globant.com>
2019-08-27 21:21:22 -0500

    Cleaned and configured project

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Clean Gatsby App</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
